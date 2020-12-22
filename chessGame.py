6a34d469a0cf574e71a7f009cf74e1ec794cadf1
Elijah Tamarchenko <elijahtamarchenko@gmail.com>
2020-12-22 10:59:13 -0500

    Simple board print

--- chessGame.py
+++ chessGame.py
@@ -8,17 +8,21 @@ class chessGame:
         
     def runGame(self):
         while not self.game.is_game_over():
-            print(self.game)
+            print(self)
             self.make_move()
-        
-        
-        
-        
-        
         
     def make_move(self):
         move = self.players[0 if self.game.turn else 1].make_move(self.game)
         self.game.push(move)  # Make the move
 
-    def is_game_over(self):
-        return self.game.is_game_over()
+    def __str__(self):
+        row = "   -----------------   \n    a b c d e f g h    \n   -----------------   \n"
+        board = str(self.game)
+        board =  board.splitlines()
+        result = row
+        for i in range (8, 0, -1):
+            result += "|"+str(i)+"| "+board[8 - i]+" |"+str(i)+"|\n"
+        result += row
+        return result
+        
+    
